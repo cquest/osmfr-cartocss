25cf05ee726c0e480360bde7f7ba9470d3a3a802
cquest <cquest@openstreetmap.fr>
2016-12-13 20:33:09 +0100

    default params table when not using mod_tile/renderd bleed patch

---
+++ params.sql
@@ -0,0 +1,4 @@
+CREATE TABLE params ( num integer, key text);
+INSERT INTO params (num, key) VALUES (0, 'buffer');
+INSERT INTO params (num, key) VALUES (0, 'x_bleed');
+INSERT INTO params (num, key) VALUES (0, 'y_bleed');
