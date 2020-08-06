a01d35d024c2d3e437b344bb9025c2f3093f0b89
Christian Quest <cquest@openstreetmap.fr>
2020-08-06 12:40:19 +0200

    download only if newer

--- get-layers.sh
+++ get-layers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # create the directory
-mkdir layers
+mkdir -p layers
 cd layers
 
 # get the layers needed by the style
 
-# z7.tif : 80 Mo
-wget http://osm13.openstreetmap.fr/%7Ecquest/z7.tif
+# z7.tif : > 80 Mo
+wget -N http://osm13.openstreetmap.fr/%7Ecquest/z7.tif
 
