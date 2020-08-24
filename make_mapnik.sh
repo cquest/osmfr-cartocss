5b6ba38abd02035a84eadcea5b0892230f91a12b
Christian Quest <cquest@openstreetmap.fr>
2020-08-24 09:54:48 +0200

    more stats

--- make_mapnik.sh
+++ make_mapnik.sh
@@ -4,4 +4,8 @@ kosmtik export osmfr.yml --mapnik-version 3.0.20 > osmfr.tmp
 python tuning/cleanxml.py osmfr.tmp > osmfr.mapnik
 rm osmfr.tmp
 tail -n1 osmfr.mapnik
+
+echo -n 'Layer: ' ; grep -c '<Layer' osmfr.mapnik
+echo -n 'StyleName: ' ; grep -c '<StyleName' osmfr.mapnik
+
 ls -l osmfr.mapnik
